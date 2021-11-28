ac9afbd02d901168ba72fd12b4f9a57140ec0246
mayuribhise <mayuribhise77@gmail.com>
2021-11-28 18:14:53 +0530

    I add new assignment

---
+++ src/Mayuri.java
@@ -0,0 +1,25 @@
+	import java.util.Scanner;
+
+public class Mayuri {
+		public static void main(String args[]){
+			Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		char ch;
+		while(t -->0)
+		{
+			ch = sc.next().charAt(0);
+			if(ch == 'B' || ch == 'b' )
+				System.out.println("Battleship");
+			else if (ch =='C' || ch =='c')
+				System.out.println("Cruiser");
+			else if (ch == 'D' || ch =='d')
+			 System.out.println("Destroyer");
+			 else if (ch =='F' || ch =='f')
+			System.out.println("Frigate");
+			
+		}
+		sc.close();
+		}
+	}
+
+
